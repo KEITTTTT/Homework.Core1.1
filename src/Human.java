2363c25beaa2d4a57b972863433832264f0233c8
Glebova <e.tulzakova@mail.ru>
2022-11-17 21:58:34 +0400

    Проверка условий добавлена.

--- src/Human.java
+++ src/Human.java
@@ -4,14 +4,32 @@ public class Human {
     int yearOfBirth;
     String jobTitle;
 
-    Human ( int yearOfBirth, String name, String town, String jobTitle){
-        this.name = name;
-        this.town = town;
-        this.yearOfBirth = yearOfBirth;
-        this.jobTitle = jobTitle;
+    Human(int yearOfBirth, String name, String town, String jobTitle) {
+        if (name == null || name.isEmpty()) {
+            this.name = " Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (town == null || town.isEmpty()) {
+            this.town = " Информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth ;
+        }
+        if (jobTitle == null || jobTitle.isEmpty()) {
+            this.jobTitle = " Информация не указана";
+
+        } else {
+            this.jobTitle = jobTitle;
+        }
+
     }
 
-    void introduce(){
+    void introduce() {
         System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году." +
                 " Я работаю на должности " + jobTitle + ". Будем знакомы!");
     }
